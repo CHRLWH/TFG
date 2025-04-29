9b6bc2dc748e25d91803e82d0990a8aeb4844063
carloshimp <carloshimp@gmail.com>
2025-04-29 12:02:33 +0000

    Funcionalidad de title screen falta diseño

--- components/titleScreen/TitleScreen.jsx
+++ components/titleScreen/TitleScreen.jsx
@@ -1,19 +1,46 @@
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:2535092692.
 import React from 'react';
+import { TouchableOpacity ,SafeAreaView, Image, StyleSheet, View, Pressable, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { ImageBackground,  ScrollView,  TouchableOpacity ,SafeAreaView, Image, StyleSheet, View, Pressable, Text, Button, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-function TitleScreen() {
-  const navigate = useNavigation();
-
-  const handleClick = () => {
-    navigate('Home');
+const onPress = () => {
+    Alert.alert('You tapped the button!');
   };
+const TitleScreen = ({}) => {
+  const navigation = useNavigation(); 
+  const [facing, setFacing] = useState<CameraType>('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+  // Obtener la navegación en un componente hijo
   return (
-    <SafeAreaView style={{justifyContent: "center",alignItemsÑ:"center"}}>
-      <Button title="GO TO HOME "onClick={handleClick}>Go to Home</Button>
+    <SafeAreaView style={{marginTop:"50%",backgroundColor: "black", flex: 0, flexDirection:"column", alignItems: "center", justifyContent:"center"}}>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Icon name="exit" size={40} color="#FFF">Inicio</Icon>
+        </TouchableOpacity> 
     </SafeAreaView>
   );
-}
+};
+
+const styles = StyleSheet.create({
+   
+});
 
 export default TitleScreen;
